7ba2a0cb998cc2254925e42a1b4db76a792ac3d9
Benoit Triay <benoit.tria+13y@gmail.com>
2023-08-14 08:44:13 +0100

    Login form

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -7,3 +8,8 @@ from app import app
 def index():
     user = {'username': 'benoit'}
     return render_template('index.html', title='Home', user=user)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign in', form=form)
