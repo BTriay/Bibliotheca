677f09649a1db5de16e9bccc2689a652c5199213
Benoit Triay <benoit.tria+13y@gmail.com>
2023-08-18 20:56:31 +0100

    models.User: add password hashing functionalities

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
@@ -13,6 +14,12 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.username}>'
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 class Post(db.Model):
     __tablename__ = 'post'
@@ -22,4 +29,4 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return f'<POst {self.body}>'
+        return f'<Post {self.body}>'
